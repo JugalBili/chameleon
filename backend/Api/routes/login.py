4cb8d413daf1249ac585eafed481e352c6eb27ea
frankyu02 <frankyu02@yahoo.ca>
2024-06-03 13:25:40 -0400

    add comments to clarify code

--- backend/Api/routes/login.py
+++ backend/Api/routes/login.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 router = APIRouter(
+    # specify subroute. All routes in this file will be in the form of /login/{whatever}
     prefix="/login",
+    # tags are stricly for metadata (helps with openAPI specifications)
     tags=["login"],
     responses={401: {"description": "Incorrect Login information"}},
 )
