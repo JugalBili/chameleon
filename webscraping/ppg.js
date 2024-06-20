38c791f2b47605729b7a95e22c4d1848739f4745
frankyu02 <frankyu02@yahoo.ca>
2024-06-20 16:07:36 -0400

    we scrape ppg paint

---
+++ webscraping/ppg.js
@@ -0,0 +1,57 @@
+import puppeteer from "puppeteer";
+import { rgbStringToArray, rgbToColorName } from "./utils.js";
+import fs from "fs/promises";
+
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+await page.goto(
+  "https://www.ppgpaints.com/color/color-families/browse-all-colors"
+);
+
+const colors = await page.$eval(
+  "section.article-section:nth-child(2)",
+  (el) => {
+    const anchorElements = Array.from(el.querySelectorAll("a"));
+
+    return anchorElements.map((a) => {
+      const painturl = a.href;
+      const colorName = a.querySelector("h3").innerText ?? "undefined";
+      const rgb = window.getComputedStyle(a).backgroundColor;
+      // genericLabel = rgbToColorName(rgb[0], rgb[1], rgb[2]);
+      const Id = a.querySelector("span").innerText ?? "undefined";
+      return {
+        brand: "PPG",
+        url: painturl,
+        rgb,
+        name: colorName,
+        Id,
+      };
+    });
+    // return anchorElements.map((a) => a.href);
+  }
+);
+if (colors) {
+  // pre-processing
+  const processedColors = colors.map((c) => {
+    const rgb = rgbStringToArray(c.rgb);
+    const genericLabel = rgbToColorName(rgb[0], rgb[1], rgb[2]);
+    return {
+      ...c,
+      rgb,
+      label: genericLabel,
+    };
+  });
+  const dataString = JSON.stringify(processedColors, null, 2);
+  try {
+    await fs.writeFile("ppg.json", dataString, "utf8");
+    console.log(
+      `Successfully Fetched data. Found ${[processedColors.length]} colors`
+    );
+  } catch (err) {
+    console.error("Error writing to file", err);
+  }
+} else {
+  console.error("Something went wrong. Could not parse the page");
+}
+
+await browser.close();
