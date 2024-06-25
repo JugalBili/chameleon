4b1c5a9c9324ac4bbb05d130e037b8c78bb424ce
Kevin Hu <67344175+kevinhu12@users.noreply.github.com>
2024-06-25 16:16:51 -0400

    updated login page

--- frontend/Chameleon/app/src/main/java/cs446/project/chameleon/LoginPage.kt
+++ frontend/Chameleon/app/src/main/java/cs446/project/chameleon/LoginPage.kt
@@ -1,54 +1,106 @@
 package cs446.project.chameleon
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.ClickableText
 import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 
 
 @Composable
 fun LoginPage(navController: NavHostController) {
+
+    val username = remember { mutableStateOf("") }
+    val password = remember { mutableStateOf("") }
+    val clicked = remember { mutableStateOf(false) }
+
+    val annotatedText = buildAnnotatedString {
+        append("Don't have an account? Sign up ")
+        pushStringAnnotation(tag = "signup", annotation = "here!")
+        withStyle(style = SpanStyle(color = Color.Blue, textDecoration = TextDecoration.Underline)) {
+            append("here!")
+        }
+        pop()
+    }
+
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-
         content = { padding ->
-            Row (
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(64.dp),
-                horizontalArrangement = Arrangement.Center
+            Column(
+                modifier = Modifier.fillMaxSize().padding(12.dp),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                Text (
-                    text = "This is the login page",
-                    modifier = Modifier.padding(48.dp)
+                Text(text = "Chameleon", fontSize = 32.sp)
+                Spacer(modifier = Modifier.height(24.dp))
+
+                OutlinedTextField(
+                    value = username.value,
+                    onValueChange = { username.value = it },
+                    label = { Text("Username") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 64.dp)
                 )
+                Spacer(modifier = Modifier.height(24.dp))
+
+                OutlinedTextField(
+                    value = password.value,
+                    onValueChange = { password.value = it },
+                    label = { Text("Password") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = 64.dp),
+                    visualTransformation = PasswordVisualTransformation()
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+
+                ClickableText(text = annotatedText, onClick = { offset ->
+                    annotatedText.getStringAnnotations(
+                        tag = "signup", start = offset, end = offset
+                    ).firstOrNull()?.let { clicked.value = true }
+                })
+
+                // TEST TO SEE IF IT WAS CLICKED
+                if (clicked.value) {
+                    Text(text = "Clicked")
+                }
             }
-
-
         },
         bottomBar = {
             Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(10.dp),
+                modifier = Modifier.fillMaxWidth().padding(10.dp),
                 horizontalArrangement = Arrangement.SpaceEvenly
             ) {
                 Button(
                     onClick = { navController.navigate("camera_screen") },
                     modifier = Modifier.padding(10.dp).weight(1f)
                 ) {
-                    Text(text = "Go to Camera Screen")
+                    Text(text = "Login")
                 }
             }
         }
-
     )
 }
