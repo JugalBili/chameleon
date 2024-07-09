6bd4ef94fb4ac930f95942b4e78fc023b08dc3f5
frankyu02 <frankyu02@yahoo.ca>
2024-07-08 22:17:52 -0400

    add env variable for storage

--- backend/Api/config.py
+++ backend/Api/config.py
@@ -3,4 +3,6 @@
 
 class Settings(BaseSettings):
     jwt_signing_key: str
+    use_firebase_emulator: str
+    firebase_storage_bucket_url: str
     model_config = SettingsConfigDict(env_file=".env")
