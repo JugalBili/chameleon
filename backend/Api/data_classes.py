748500ccd2a815faa3d069f06d00d0c87b37925b
frankyu02 <frankyu02@yahoo.ca>
2024-07-10 14:16:40 -0400

    add image repo classes into shared file

--- backend/Api/data_classes.py
+++ backend/Api/data_classes.py
@@ -10,26 +10,3 @@ class UserLoginDto(BaseModel):
 class CreateUserDTO(UserLoginDto):
     firstname: str
     lastname: str
-
-
-class RGB(BaseModel):
-    r: int
-    g: int
-    b: int
-
-
-class ImageUploadDTO(BaseModel):
-    paintId: str
-    color: RGB
-
-
-class Image(BaseModel):
-    image_bytes: bytes
-    contentType: str
-
-
-class GetImageResponse(BaseModel):
-    image_hash: str
-    rgb: RGB | None
-    paintId: str | None
-    image_data: Image | None = None
---
+++ backend/shared/data_classes.py
@@ -0,0 +1,24 @@
+﻿from pydantic import BaseModel
+
+
+class RGB(BaseModel):
+    r: int
+    g: int
+    b: int
+
+
+class ImageUploadDTO(BaseModel):
+    paintId: str
+    color: RGB
+
+
+class Image(BaseModel):
+    image_bytes: bytes
+    contentType: str
+
+
+class GetImageResponse(BaseModel):
+    image_hash: str
+    rgb: RGB | None
+    paintId: str | None
+    image_data: Image | None = None
