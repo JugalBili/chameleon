319465f81c9b933905c3f4def665201e3f88f8df
Brian Youngseok Chae <brian.chae.ys@gmail.com>
2024-07-21 22:07:18 -0400

    Add image to review card

--- frontend/Chameleon/app/src/main/java/cs446/project/chameleon/composables/ReviewCard.kt
+++ frontend/Chameleon/app/src/main/java/cs446/project/chameleon/composables/ReviewCard.kt
@@ -1,10 +1,13 @@
 package cs446.project.chameleon.composables
 
+import android.graphics.BitmapFactory
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -15,7 +18,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import cs446.project.chameleon.R
 import cs446.project.chameleon.composables.styling.ChameleonDivider
 import cs446.project.chameleon.composables.styling.ChameleonText
 import cs446.project.chameleon.data.model.Review
@@ -28,6 +34,8 @@ fun ReviewCard(review: Review) {
 
     // TODO: query the user associated with the review.uid to get the name
     val name = "John UninspiredLastName"
+    // Test image
+    val image = BitmapFactory.decodeResource(LocalContext.current.resources, R.drawable.demo_before)
 
     Card(
         modifier = Modifier.fillMaxWidth().padding(12.dp),
@@ -46,6 +54,14 @@ fun ReviewCard(review: Review) {
 
             ChameleonDivider()
             ChameleonText(review.review, BODY)
+
+            Image(
+                bitmap = image.asImageBitmap(),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .align(Alignment.Start)
+            )
         }
     }
 }
